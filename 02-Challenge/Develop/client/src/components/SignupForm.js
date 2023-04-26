20d5c8585ae539f7b82a1ef14b9aa9da7647d009
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-04-26 18:53:34 -0400

    updated app js

--- 02-Challenge/Develop/client/src/components/SignupForm.js
+++ 02-Challenge/Develop/client/src/components/SignupForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 const SignupForm = () => {
   // set initial form state
@@ -11,6 +11,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [ createUser ] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,7 +29,8 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await createUser({
+        variables: {...userFormData} });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
