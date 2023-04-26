830f8d08c8c5ae566332a0c7b29ad045c6648302
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-04-26 16:38:11 -0400

    json packages in order working on type defs and resolvers

---
+++ 02-Challenge/Develop/server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    name: String
+    email: String
+    password: String
+    savedBooks: [String]!
+  }
+
+  type Auth {
+    token: ID!
+    profile: Profile
+  }
+
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
+    me: User
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    // addSkill(profileId: ID!, skill: String!): Profile
+    // removeProfile: Profile
+    // removeSkill(skill: String!): Profile
+  }
+`;
+
+module.exports = typeDefs;
